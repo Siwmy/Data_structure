22078572b2a9896582154d7a172ab5707954edaa
Swimy <2986437402@qq.com>
2026-04-10 15:30:00 +0800

    第一次提交：顺序表代码

---
+++ vector.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+/*
+vector是C++标准库提供的一个动态数组容器，具有以下特点：
+1.动态大小：vector可以根据需要自动调整大小，允许在运行时添加或删除元素。
+2.连续内存：vector在内存中以连续的方式存储元素，这使得它在访问元素时具有较高的效率。
+3.随机访问：vector支持通过索引进行随机访问，可以直接访问任意位置的元素。
+用法示例：vector<数据类型> 名称；
+函数用法：
+1.push_back(value)：在vector末尾添加一个元素。
+2.size()：返回vector中元素的数量。
+3.capacity()：返回vector当前的容量，即在不进行扩容的情况下可以存储的元素数量。
+4.at(index)：返回vector中指定位置的元素，并进行边界检查。
+*/
+using namespace std;
+int main() {
+	vector<int> ret = { 1,2,3,4,5 };
+	ret.push_back(60);
+	for (int i = 0;i < ret.size();i++) {
+		cout << ret[i] << " ";
+	}
+	cout << ret.at(2) << endl;
+}
---
+++ 手写顺序表.cpp
@@ -0,0 +1,105 @@
+﻿#include<iostream>
+#include<cassert>
+using namespace std;
+/*
+1.检查索引位置是否合法
+2.检查顺序表是否已满，如果已满则需要扩容，可以扩容两倍大小
+3.将插入位置之后的元素依次后移
+4.将新元素插入到指定位置
+*/
+
+struct SequentialList
+{
+	int* data;
+	int size;
+	int capacity;
+	SequentialList(int size) {
+		this->size = 0;
+		this->capacity = size;
+		this->data = new int[size];
+	}
+	~SequentialList(){
+		delete[] data;
+		cout << "顺序表已销毁" << endl;
+	}
+
+	void insert(int index, int value) {  //插入
+		if (index<0 || index>size)
+		{
+			cout << "索引位置不合法" << endl;
+			return;
+		}
+		if (size == capacity) {
+			int newcapacity = capacity * 2;
+			if (newcapacity == 0) newcapacity = 1; //处理初始容量为0的极端情况
+			int* newdata = new int[newcapacity];
+			for (int i = 0;i < size;i++) {
+				newdata[i] = data[i];
+			}
+			delete[] data;
+			data = newdata;
+			capacity = newcapacity;
+		}
+		
+		for (int i = size;i > index;i--) {
+			data[i] = data[i - 1];
+		}
+		data[index] = value;
+		size++;
+		
+	}
+
+	void deleteList(int index) {
+		if (index < 0 || index >= size) {
+			cout << "索引位置不合法" << endl;
+			return;
+		}
+		for (int i = index;i < size - 1;i++) {
+			data[i] = data[i + 1];
+		}
+		size--;
+	}
+
+	void change(int index, int value) {
+		if (index < 0 || index >= size) {
+			cout << "索引位置不合法" << endl;
+			return;
+		}
+		data[index] = value;
+	}
+	int find(int value)
+	{
+		for (int i = 0;i < size;i++)
+		{
+			if (data[i] == value)
+				return i;
+		}
+		return -1;
+	}
+};
+
+
+int main1()
+{
+	SequentialList list(10);
+	cout << list.capacity << " " << list.size << endl;
+	for (int i = 0;i < 10;i++)
+		list.insert(i, i * 10);
+	for (int i = 0;i < list.size;i++)
+		cout << list.data[i] << " ";
+	cout << endl;
+	list.change(2, 520);
+	for (int i = 0;i < list.size;i++)
+		cout << list.data[i] << " ";
+	cout << endl;
+	list.insert(6, 1090);
+	cout << list.capacity << " " << list.size << endl;
+	for (int i = 0;i < list.size;i++)
+		cout << list.data[i] << " ";
+	cout << endl;
+	list.deleteList(4);
+	for (int i = 0;i < list.size;i++)
+		cout << list.data[i] << " ";
+	cout << endl;
+	return 0;
+}
